d5529ab66521b7624103f09bdaacfaa7bd442f66
Bayu Maulana <bayufransdo@gmail.com>
2023-12-18 23:51:29 +0700

    checkpoint: X can appear now

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
